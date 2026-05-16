106aef2e5e1c827fe0023d2ba3c09c5fdfe222da
tequi <tequi@DESKTOP-17N706G>
2026-05-16 00:46:57 -0600

    VERSION VACIA

---
+++ src/conexion/CreateConection.java
@@ -0,0 +1,39 @@
+package conexion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class CreateConection {
+    static Properties config = new Properties();
+    String hostname, port, database, username, password;
+
+    public CreateConection() {
+        String path = "C:\\Users\\tequi\\OneDrive\\Documentos\\NetBeansProjects\\Speedrun2\\src\\conexion\\dbproduccion.properties";
+        try {
+            config.load(Files.newInputStream(Paths.get(path)));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        loadProperties();
+    }
+
+    public void loadProperties() {
+        this.hostname = config.getProperty("hostname");
+        this.port = config.getProperty("port");
+        this.database = config.getProperty("database");
+        this.username = config.getProperty("username");
+        this.password = config.getProperty("password");
+    }
+
+    public Connection getConnection() throws SQLException {
+        String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + database;
+        
+        System.out.println("Conectado exitosamente");
+        return DriverManager.getConnection(jdbcUrl, username, password);
+    }
+}
